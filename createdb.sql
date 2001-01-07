0e37e53f6ca41412f1795b9ee002fb58cfa70902
Per Cederqvist <ceder@lysator.liu.se>
2001-01-07 19:32:15 +0000

    (key_info.key_type): Column removed. (tasks.finished, tasks.lastused, tasks.pathfound): New columns. (task_trusted, task_target, keys_needed): New tables.

--- createdb.sql
+++ createdb.sql
@@ -12,17 +12,22 @@ use pgppathfinder;
 # unique.  This may cause us to report false paths.  Big deal.  The
 # end user *must* check the signatures to get any security, and the
 # false path will be detected than.
+#
+# We don't care about which uid a signator signed.  If he has signed
+# any uid, the signature is good for all of them, as far as we are
+# concerned.
+
 create table key_info (
-	# Info about a key.
 	key_id		uint32		not null,
 
 	# We store NULL in the fields below if we know for sure that
 	# the key servers don't know anything about this key.
-	key_type	char(8)		null, # "1024R"
+
 	creation_date	date		null,
 
 	# When was the above info, and corresponding info in key_sigs
 	# and key_uids, last checked with a key server?
+
 	sigs_updated	datetime	not null,
 
 	INDEX(key_id)
@@ -38,7 +43,7 @@ create table key_uids (
 create table key_sigs (
 	key_id		uint32		not null,
 	signed_by	uint32		not null, # May refer to a key
-						  # we know nothing about.
+						  # we know nothing about yet.
 
 	INDEX(key_id),
 	INDEX(signed_by)
@@ -51,6 +56,40 @@ create table tasks (
 	trusted		uint64		not null,
 	fetched		uint32		not null,
 	created		datetime	not null,
+	finished	datetime	null,
+	lastused	datetime	null,
+	pathfound	bool		null,
 
-	INDEX(taskno)
+	INDEX(taskno),
+	INDEX(target, trusted)
+);
+
+create table task_trusted (
+	taskno		uint64		not null,
+	key_id		uint64		not null,
+	signed_by	uint64		not null,
+	distance	uint8		not null,
+
+	INDEX(taskno),
+	INDEX(key_id),
+	INDEX(signed_by)
+);
+
+create table task_target (
+	taskno		uint64		not null,
+	key_id		uint64		not null,
+	signed_by	uint64		not null,
+	distance	uint8		not null,
+
+	INDEX(taskno),
+	INDEX(key_id),
+	INDEX(signed_by)
+);
+
+create table keys_needed (
+	taskno		uint64		not null,
+	key_id		uint64		not null,
+	distance	uint8		not null,
+
+	INDEX(taskno, distance)
 );
