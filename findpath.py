8327f2f7e6f3f908f5aa2bb40f3e0bde66c459c5
Per Cederqvist <ceder@lysator.liu.se>
2001-01-09 08:40:50 +0000

    (find_path): This now basically works, but only if all keys are 	already present in the database.  Vast heuristical 	improvements on the algorithm are possible. (print_path): New function.

--- findpath.py
+++ findpath.py
@@ -3,7 +3,9 @@ import socket
 import pgppathconfig
 import pathdb
 
-def findpath(target, trusted):
+def find_path(target, trusted):
+    assert target > 0
+    assert trusted > 0
     task = pathdb.create_task(target, trusted)
     keyfetcher = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     keyfetcher.connect(('localhost', pgppathconfig.keyfetcher_port))
@@ -15,17 +17,42 @@ def findpath(target, trusted):
     if line != "hello\n":
         print "Bad hello from keyserver:", line
 
-    # FIXME: the rest of this functions is just demo code.
-    if pathdb.need_key(task, target, 1) == 0:
-        key = keyfetcher.recv(64)
-        if long(key, 16) != target:
-            print "Bad key info", key
-        else:
-            print "Target fetched OK"
-    else:
-        print "Target already present in database"
 
+    # Get the start- and endpoint.  We really, really need them.
+    pending = []
+    if pathdb.need_key(task, target, 0) == 0:
+        pending.append(target)
+    if target != trusted and pathdb.need_key(task, trusted, 0) == 0:
+        pending.append(trusted)
+    while len(pending) > 0:
+        pending.remove(long(keyfetcher.recv(64), 16))
+
+    target_dist = 0
+    trusted_dist = 0
+    pathdb.initial_setup(task, target, trusted)
+    while 1:
+        path = pathdb.best_match_found(task, target_dist, trusted_dist)
+        if path != None:
+            return path
+        new_entries = pathdb.extend_target(task, target_dist)
+        if new_entries == 0:
+            return None
+        target_dist += 1
+
+
+def print_path(path):
+    if path == None:
+        print "No match found"
+    else:
+        for k in path:
+            print "0x%08X" % k,
+        print
 
 if __name__ == '__main__':
     pathdb.init()
-    findpath(0x12345678, 0x12345678)
+    print "Starting"
+    print_path(find_path(0x12345678L, 0x12345678L))
+    print_path(find_path(0xF9036141L, 0xF9036141L))
+    print_path(find_path(0x9AA2E311L, 0x94514BBDL))
+    print_path(find_path(0x9AA2E311L, 0x57D395E1L))
+    print "Done"
